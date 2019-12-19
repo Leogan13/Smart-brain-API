ea2627c942ad440ca0371cecbb4b81c13a5e3f98
Leogan <leogan1399@gmail.com>
2019-12-19 00:06:50 -0500

    changing response

--- server.js
+++ server.js
@@ -39,27 +39,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-const database = {// temporal database for users testing
-	users: [
-		{
-			id:'123',
-			name:'John',
-			email: 'john@gmail.com',
-			password: 'cookies',
-			entries: 0,
-			joined: new Date()
-		},
 
-		{
-			id:'124',
-			name:'Sally',
-			email: 'sally@gmail.com',
-			password: 'bananas',
-			entries: 0,
-			joined: new Date()
-		}
-	]
-}
 
 
 
